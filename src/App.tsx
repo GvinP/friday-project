c25ba126d49d0f0dd708491b6c087b9914e47b85
GvinP <gvinpin72@gmail.com>
2022-06-06 15:20:32 +0400

    add pages and store

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Navigate, Route, Routes} from 'react-router-dom';
+import Profile from "./components/Profile";
+import Login from "./components/Login";
+import Registration from "./components/Registration";
+import Page404 from "./components/Page404";
+import PasswordRecovery from "./components/PasswordRecovery";
+import PasswordNew from "./components/PasswordNew";
+import Examples from "./components/Examples";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path={'/'} element={<Navigate to={'/profile'}/>}/>
+        <Route path={'/profile'} element={<Profile/>}/>
+        <Route path={'/login'} element={<Login/>}/>
+        <Route path={'/registration'} element={<Registration/>}/>
+        <Route path={'/404'} element={ <Page404/>}/>
+        <Route path={'/passwordrecovery'} element={<PasswordRecovery/>}/>
+        <Route path={'/passwordnew/'} element={<PasswordNew/>}/>
+        <Route path={'/tests'} element={<Examples/>}/>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Examples.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import SuperButton from "../common/c2-SuperButton/SuperButton";
+import SuperInputText from "../common/c1-SuperInputText/SuperInputText";
+import SuperCheckbox from "../common/c3-SuperCheckbox/SuperCheckbox";
+
+const Examples = () => {
+    return (
+        <div>
+            <SuperCheckbox/>
+            <SuperInputText/>
+            <SuperButton>Button</SuperButton>
+        </div>
+    );
+};
+
+export default Examples;
