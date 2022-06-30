fa8c0f9a4580aa67851947c8385b3169ec7c8888
Kattesh <kate.sh@icloud.com>
2022-06-30 14:07:05 -0500

    some changes

--- src/common/EditableSpan.tsx
+++ src/common/EditableSpan.tsx
@@ -1,5 +1,6 @@
 import React, { ChangeEvent, useState } from 'react';
 import TextField from '@mui/material/TextField';
+import {InputText} from "./InputText/InputText";
 
 type EditableSpanPropsType = {
     value: string
@@ -7,7 +8,7 @@ type EditableSpanPropsType = {
 }
 
 export const EditableSpan = React.memo(function (props: EditableSpanPropsType) {
-    console.log('EditableSpan called');
+
     let [editMode, setEditMode] = useState(false);
     let [title, setTitle] = useState(props.value);
 
@@ -24,6 +25,6 @@ export const EditableSpan = React.memo(function (props: EditableSpanPropsType) {
     }
 
     return editMode
-        ? <TextField value={title} onChange={changeTitle} autoFocus onBlur={activateViewMode}/>
+        ? <InputText value={title} onChange={changeTitle} autoFocus onBlur={activateViewMode}/>
         : <span onDoubleClick={activateEditMode}>{props.value}</span>
 });
